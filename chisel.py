4a572026ecd7678e52029f624eb4542c8335b292
Alex Le <alex.csm@gmail.com>
2012-02-06 22:14:29 -0700

    redesign page layout to 1 column

--- chisel.py
+++ chisel.py
@@ -7,7 +7,7 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 # Settings
 SOURCE = "./posts/"
 DESTINATION = "./blog/"
-HOME_SHOW = 25
+HOME_SHOW = 15
 TEMPLATE_PATH = "./templates/"
 TEMPLATE_OPTIONS = {}
 TEMPLATES = {
